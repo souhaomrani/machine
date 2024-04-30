10d4ba3f5eda5f4cfdd9c28ca3ec89c388921422
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-30 02:38:39 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,93 +1,26 @@
-variable "api_token" {
-  description = "Token to connect Proxmox API"
-  type = string
+variable "pm_api_url" {
+  description = "URL de l'API Proxmox"
+  default     = "https://192.168.127.134:8006/api2/json"  # Remplacez par l'URL de votre API Proxmox
 }
 
+variable "pm_user" {
+  description = "Nom d'utilisateur Proxmox"
+  default     = "root@pam!souhasouha"  # Remplacez par votre nom d'utilisateur Proxmox
+}
+
+variable "pm_password" {
+  description = "Mot de passe Proxmox"
+  default     = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"  # Remplacez par votre mot de passe Proxmox
+}
+
+variable "template" {
+  description = "Nom du modèle pour la création de la machine virtuelle"
+  type        = string
+  default     = "ubuntu.robert.local"  # Remplacez par le nom de votre modèle de machine virtuelle Proxmox
+}
 
 variable "target_node" {
-  description = "Proxmox node"
-  type        = string
-  default = "devbox1"
+  description = "Nœud Proxmox sur lequel déployer la machine virtuelle"
+  default     = "pve"  # Remplacez par le nom de votre nœud Proxmox cible
 }
 
-variable "onboot" {
-  description = "Auto start VM when node is start"
-  type        = bool
-  default     = true
-}
-
-variable "target_node_domain" {
-  description = "Proxmox node domain"
-  type        = string
-  default = ""
-}
-
-variable "vm_hostname" {
-  description = "VM hostname"
-  type        = string
-  default = "test"
-}
-
-variable "domain" {
-  description = "VM domain"
-  type        = string
-  default = "robert.local"
-}
-
-variable "vm_tags" {
-  description = "VM tags"
-  type        = list(string)
-  default = [ "ubuntu" ]
-}
-
-variable "template_tag" {
-  description = "Template tag"
-  type        = string
-  default = "test"
-}
-
-variable "sockets" {
-  description = "Number of sockets"
-  type        = number
-  default     = 1
-}
-
-variable "cores" {
-  description = "Number of cores"
-  type        = number
-  default     = 1
-}
-
-variable "memory" {
-  description = "Number of memory in MB"
-  type        = number
-  default     = 2048
-}
-
-variable "vm_user" {
-  description = "User"
-  type        = string
-  sensitive   = true
-  default = "sysadmin"
-}
-
-variable "disk" {
-  description = "Disk (size in Gb)"
-  type = object({
-    storage = string
-    size    = number
-  })
-  default = {
-    storage = "local"
-    size = 20
-  }
-}
-
-variable "additionnal_disks" {
-  description = "Additionnal disks"
-  type = list(object({
-    storage = string
-    size    = number
-  }))
-  default = []
-}
