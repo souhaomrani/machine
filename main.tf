a3cca7597715a725820d669fa4f6e1d5b0c3ee18
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-30 02:49:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
 provider "proxmox" {
   pm_api_url         = "https://192.168.127.134:8006/api2/json"
   pm_api_token_id    = "root@pam!souhasouha"
