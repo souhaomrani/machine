20d5a4919140d9610f902632bd2f14a6f4aed6a8
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-04-30 02:24:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,60 +1,58 @@
-data "proxmox_virtual_environment_vms" "template" {
-  node_name = var.target_node
-  tags      = ["template", var.template_tag]
+provider "proxmox" {
+  pm_api_url         = "https://192.168.127.134:8006/api2/json"
+  pm_api_token_id    = "root@pam!souhasouha"
+  pm_api_token_secret = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"
+  pm_tls_insecure    = true  # Ajustez cette option en fonction de votre configuration de sécurité
 }
 
+data "proxmox_virtual_environment_vms" "template" {
+  node_name = "pve"
+  tags      = ["template", "test"]  # Remplacez "test" par vos propres tags de template
+}
 
 resource "proxmox_virtual_environment_file" "cloud_user_config" {
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = var.target_node
+  node_name    = "pve"
 
   source_raw {
-    data = file("cloud-init/user_data")
-
-    file_name = "${var.vm_hostname}.${var.domain}-ci-user.yml"
+    data      = file("cloud-init/user_data")  # Assurez-vous que le chemin du fichier cloud-init est correct
+    file_name = "ubuntu.${var.domain}-ci-user.yml"  # Remplacez "ubuntu" et "${var.domain}" par vos valeurs spécifiques
   }
 }
 
 resource "proxmox_virtual_environment_file" "cloud_meta_config" {
   content_type = "snippets"
   datastore_id = "local"
-  node_name    = var.target_node
+  node_name    = "pve"
 
   source_raw {
-    data = templatefile("cloud-init/meta_data",
-      {
-        instance_id    = sha1(var.vm_hostname)
-        local_hostname = var.vm_hostname
-      }
-    )
-
-    file_name = "${var.vm_hostname}.${var.domain}-ci-meta_data.yml"
+    data      = templatefile("cloud-init/meta_data.tpl", { instance_id = sha1(var.vm_hostname), local_hostname = var.vm_hostname })
+    file_name = "ubuntu.${var.domain}-ci-meta_data.yml"  # Remplacez "ubuntu" et "${var.domain}" par vos valeurs spécifiques
   }
 }
 
 resource "proxmox_virtual_environment_vm" "vm" {
-  name      = "${var.vm_hostname}.${var.domain}"
-  node_name = var.target_node
+  name      = "ubuntu.${var.domain}"
+  node_name = "pve"
 
-  on_boot = var.onboot
-
+  on_boot = true  # Démarre automatiquement la VM après le déploiement
 
   agent {
     enabled = true
   }
 
-  tags = var.vm_tags
+  tags = ["test"]  # Remplacez "test" par vos propres tags pour la VM
 
   cpu {
     type    = "x86-64-v2-AES"
-    cores   = var.cores
-    sockets = var.sockets
+    cores   = 2
+    sockets = 1
     flags   = []
   }
 
   memory {
-    dedicated = var.memory
+    dedicated = 2048  # Taille de la mémoire en Mo
   }
 
   network_device {
@@ -62,9 +60,6 @@ resource "proxmox_virtual_environment_vm" "vm" {
     model   = "virtio"
   }
 
-  # Ignore changes to the network
-  ## MAC address is generated on every apply, causing
-  ## TF to think this needs to be rebuilt on every apply
   lifecycle {
     ignore_changes = [
       network_device,
@@ -77,39 +72,15 @@ resource "proxmox_virtual_environment_vm" "vm" {
   disk {
     interface    = "scsi0"
     iothread     = true
-    datastore_id = "${var.disk.storage}"
-    size         = var.disk.size
+    datastore_id = "local"
+    size         =   20  # Taille du disque en Go
     discard      = "ignore"
   }
 
-  dynamic "disk" {
-    for_each = var.additionnal_disks
-    content {
-      interface    = "scsi${1 + disk.key}"
-      iothread     = true
-      datastore_id = "${disk.value.storage}"
-      size         = disk.value.size
-      discard      = "ignore"
-      file_format  = "raw"
-    }
-  }
-
-  clone {
-    vm_id = data.proxmox_virtual_environment_vms.template.vms[0].vm_id
-  }
-
   initialization {
-    # ip_config {
-    #   ipv4 {
-    #     address = "dhcp"
-    #   }
-    # }
-
     datastore_id         = "local"
     interface            = "ide2"
     user_data_file_id    = proxmox_virtual_environment_file.cloud_user_config.id
     meta_data_file_id    = proxmox_virtual_environment_file.cloud_meta_config.id
   }
-
-
 }
