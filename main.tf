5e5a1cef957e067a88ea44545f8d92a2ef01e01d
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-07 22:13:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,16 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url         = "https://192.168.127.134:8006/api2/json"
-  pm_api_token_id    = "root@pam!souhasouha"
-  pm_api_token_secret = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"
-  pm_tls_insecure    = true  # Ajustez cette option en fonction de votre configuration de sécurité
+  pm_api_url         = var.pm_api_url
+  pm_api_token_id    = var.pm_user
+  pm_api_token_secret = var.pm_password
+  pm_tls_insecure    = true
 }
 
 resource "proxmox_vm_qemu" "ubuntu_vm" {
   name        = "ubuntu-vm"
   clone       = var.template
-  target_node = "pve"
+  target_node = var.target_node
   memory      = 2048
   cores       = 2
 
@@ -31,10 +31,8 @@ resource "proxmox_vm_qemu" "ubuntu_vm" {
     storage      = "local"
     size         = "20G"
     type         = "scsi"
-    iothread     = 1  # Activer iothread (1) ou le désactiver (0)
+    iothread     = 1
     storage_type = "raw"
     filename     = "local-lvm:vm-100-disk-0"
   }
 }
-
-
