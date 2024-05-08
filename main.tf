3fb8e748bd380feacaa3fc8f35c08759d1fd776e
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-08 20:08:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,35 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
+      source  = "Telmate/proxmox"
       version = "3.0.1-rc1"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url         = var.pm_api_url
-  pm_api_token_id    = var.pm_user
-  pm_api_token_secret = var.pm_password
-  pm_tls_insecure    = true
+  pm_api_url          = "https://192.168.127.134:8006/api2/json"
+  pm_user             = "root@pam!souhasouha"
+  pm_password         = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"
 }
 
-resource "proxmox_vm_qemu" "ubuntu_vm" {
-  name        = "ubuntu-vm"
-  clone       = var.template
-  target_node = var.target_node
-  memory      = 2048
-  cores       = 2
+resource "proxmox_vm_qemu" "terraform-test" {
+  name         = "ubuntu.robert.local"  # Nom de la machine virtuelle
+  target_node  = "pve"                  # Noeud Proxmox sur lequel déployer la machine virtuelle
+  clone        = "9999"       # Modèle à cloner pour la machine virtuelle
+  memory       = 2048                   # Mémoire assignée à la machine virtuelle (en Mo)
+  cores        = 2                      # Nombre de cœurs de processeur assignés à la machine virtuelle
 
   network {
-    model  = "virtio"
-    bridge = "vmbr0"
+    model  = "virtio"                   # Modèle de carte réseau
+    bridge = "vmbr0"                    # Pont réseau associé
+  }
+
+  disk {
+    id           = 0                     # ID du disque
+    storage      = "local-lvm"           # Stockage pour le disque
+    size         = "20G"                 # Taille du disque principal
+    type         = "scsi"                # Type de contrôleur de disque
+    filename     = "local:9999/vm-9999-cloudinit.qcow2"  # Chemin vers le disque cloud-init
   }
 }
