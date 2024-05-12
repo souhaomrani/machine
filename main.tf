0a9bff80d5839fce30a1f9383ff2ee0471317cf8
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-12 17:31:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,26 +9,26 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://192.168.127.134:8006/api2/json"
-  pm_user             = "root@pam!souhasouha"
-  pm_password         = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"
+  pm_api_url     = var.pm_api_url
+  pm_user        = var.pm_user
+  pm_password    = var.pm_password
 }
 
 resource "proxmox_vm_qemu" "terraform-test" {
-  target_node  = "pve"                    # Hôte Proxmox sur lequel créer la machine virtuelle
-  hostname     = "terraform-test"         # Nom de la machine virtuelle
-  ostemplate   = "local:vztmpl/almalinux-9-default_20221108_amd64.tar.xz"  # Chemin vers le modèle de la machine virtuelle
-  password     = "terraform"              # Mot de passe de la machine virtuelle
-  unprivileged = true                     # Exécuter le processus de la machine virtuelle sans privilèges
+  target_node  = var.target_node
+  hostname     = "terraform-test"
+  ostemplate   = "local:vztmpl/almalinux-9-default_20221108_amd64.tar.xz"
+  password     = "terraform"
+  unprivileged = true
 
   rootfs {
-    storage = "local-lvm"   # Stockage pour le système de fichiers racine de la machine virtuelle
-    size    = "5G"          # Taille du système de fichiers racine
+    storage = "local-lvm"
+    size    = "5G"
   }
 
   network {
-    name   = "eth0"     # Nom de l'interface réseau dans la machine virtuelle
-    bridge = "vmbr0"    # Pont réseau associé
-    ip     = "dhcp"     # Attribution IP via DHCP
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
   }
 }
