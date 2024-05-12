4d68715c572d25fe58e4a11db23b8a8f1e91b468
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-12 18:12:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Configuration Terraform
 terraform {
   required_providers {
     proxmox = {
@@ -7,25 +8,28 @@ terraform {
   }
 }
 
+# Déclaration du fournisseur Proxmox
 provider "proxmox" {
   pm_api_url  = var.pm_api_url
   pm_user     = var.pm_user
   pm_password = var.pm_password
 }
 
+# Déclaration de la ressource Proxmox VM QEMU (machine virtuelle)
 resource "proxmox_vm_qemu" "terraform-test" {
-  target_node = "pve"
+  target_node = var.target_node
   vmid        = 9999
-  name        = "ubuntu.robert.local"
-  os_type     = "ubuntu"  # Assurez-vous d'adapter cette valeur à votre OS
+  name        = var.template
+  os_type     = "ubuntu"
   clone       = true
   full_clone  = false
-  storage     = "local-lvm"  # Assurez-vous de spécifier le stockage approprié
+  storage     = "local-lvm"
 
   network {
     model  = "virtio"
-    bridge = "vmbr0"  # Assurez-vous d'utiliser le bon bridge réseau
+    bridge = "vmbr0"
   }
 
-  # Spécifiez d'autres configurations de la machine virtuelle si nécessaire
+  # Configuration cloud-init pour la machine virtuelle
+  user_data = file("${path.module}/cloud-init-config.yaml")
 }
